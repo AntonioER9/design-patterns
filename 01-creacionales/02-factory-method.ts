ab4dde2915eba19fe33ce6e9ab52c0e477ac7d06
AntonioER9 <aespinozareyes9@gmail.com>
2025-01-07 09:54:04 -0300

    Implement Factory Method pattern for hamburger creation with Chicken and Beef options

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,62 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Chicken Hamburger');
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Beef Hamburger');
+  }
+}
+
+abstract class Restaurant {
+  abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt('Que tipo de hamburguesa desea? (chicken/beef/bean)');
+  switch (burgerType) {
+    case 'chicken':
+      restaurant = new ChickenRestaurant();
+      break;
+
+    case 'beef':
+      restaurant = new BeefRestaurant();
+      break;
+
+    default:
+      throw new Error('Tipo de hamburguesa no soportada');
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
