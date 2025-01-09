3afa77ee7681d02cd3c29a6fdac7962835d71556
Antonio Espinoza (Chile) <a0e0bhj@m-p3j23wmljw.homeoffice.wal-mart.com>
2025-01-09 15:22:12 -0300

    Add factory function for multilingual greeter; implement main function for demonstration

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,29 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+type Language = 'es' | 'en' | 'pt';
+
+//i18n
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: 'Hola',
+      en: 'Hello',
+      pt: 'Olá',
+    }
+
+    return console.log(`${messages[lang]} ${name}`);
+  };
+}
+
+function main() {
+  const spanishGreeter = createGreeter('es');
+  const englishGreeter = createGreeter('en');
+  const portugueseGreeter = createGreeter('pt');
+
+  spanishGreeter('Jorge');
+  englishGreeter('Jorge');
+  portugueseGreeter('Jorge');
+}
+
+main();
